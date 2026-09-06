cc04d3f9d98ac242200770f64c0ebb77d2bdbbde
aryan17705 <aryanpatil.developer@gmail.com>
2026-09-06 22:19:22 +0530

    Update page.tsx

--- src/app/wizard/page.tsx
+++ src/app/wizard/page.tsx
@@ -1534,7 +1534,7 @@ export default function WizardPage() {
                                 assessmentPurpose:
                                   "small-project",
                                 purpose:
-                                  "small-project",
+                                  "business",
                                 activityType:
                                   "",
                               })
